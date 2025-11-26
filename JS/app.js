5d6ffe9ce3b1dd98a7ca33b97fedbb15f665b366
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-11-26 11:25:40 +0530

    your commit message

---
+++ JS/app.js
@@ -0,0 +1,5 @@
+let marks = 90;
+ if ((marks>=44 && marks>= 88)){
+    console.log("Pass");
+    console.log("A");
+ }
--- Practice.js
+++
@@ -1,5 +0,0 @@
-let rbuf = Buffer.alloc(26);
-for (var i = 65, j = 0; i <= 90; i++, j++) {
-  rbuf[j] = i;
-}
-console.log(rbuf.toString('ascii')); 
