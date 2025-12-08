322e10f6fe7b9fc135d04f417195d47b337d1d21
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-08 22:35:58 +0530

    Updated files

--- JS/JS7.js
+++ JS/JS7.js
@@ -1,19 +1,41 @@
-const square = (n) => n*n;
-console.log(square(6));
+// const square = (n) => n*n;
+// console.log(square(6));
 
 
  
-let id = setInterval (() =>{
-    console.log("Hello World");
+// let id = setInterval (() =>{
+//     console.log("Hello World");
 
-},2000);
+// },2000);
 
-setTimeout(() => {
-    clearInterval(id);
-    console.log("Clear Interval Ran")
-},10000);
+// setTimeout(() => {
+//     clearInterval(id);
+//     console.log("Clear Interval Ran")
+// },10000);
 
 
-let sum = function (a ,b){
-    return a + b ;
-}
+// let sum = function (a ,b){
+//     return a + b ;
+// }
+
+// const student={
+//     name : "prasad",
+//     age : 23,
+//     eng : 90,
+//     math:60,
+//     phy:50,
+//     getAvg(){
+//         console.log(this);
+//         let avg = (this.eng + this.math + this.phy)/3;
+//         console.log(avg);
+//     }
+
+// }
+
+// const pow = (a,b) =>{
+//     return a ** b;
+// }
+// let p = pow(2,3);
+// console.log(p);
+
+// const sum = (a,b) => (a+b);
